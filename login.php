8352c5b145b9a1b6eee00c853284a2bf3c89f490
fSabay <133850240+fSabay@users.noreply.github.com>
2023-06-07 15:18:19 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,80 @@
+<?php
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    if ($user) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: index.php");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+	
+	<style>
+	div {
+	float:right;
+	}
+	mid{
+	float:center;
+	}
+	article {
+	float:left;
+	}
+	</style>
+</head>
+<body>
+    
+    <h1>Login</h1>
+    
+    <?php if ($is_invalid): ?>
+        <em>Invalid login</em>
+    <?php endif; ?>
+    
+    <form method="post">
+        <label for="email">email</label>
+        <input type="email" name="email" id="email"
+               value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+		
+		<a href="signup.html">Don't have an account?</a>
+        <br>
+        <button>Log in</button>
+    </form>
+	<div>
+    <a href="about.html"><img src="C:\xampp\htdocs\IT_ADET\form\portal.png" alt="about.html" style="width:500px;height:600px;"></a>
+	</div>
+    
+</body>
+</html>
